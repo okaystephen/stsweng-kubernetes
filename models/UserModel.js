aa7ffdcee5e46c273875aeb4b7749d4a8c927021
iravillanuevaa <ira_villanueva@dlsu.edu.ph>
2020-11-12 14:21:00 +0800

    added UserModel

---
+++ models/UserModel.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        first: String,
+        middle: String,
+        last: String,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    birthdate: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    eContactPerson: {
+        type: String,
+        required: true
+    },
+    eContactNum: {
+        type: String,
+        required: true,
+    },
+    medhistory: {
+        type: mongoose.Types.ObjectId,
+        ref: 'MedHistory'
+    },
+    appointments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Appointment'
+    }],
+    programs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Program'
+    }],
+    created: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('User', UserSchema);
