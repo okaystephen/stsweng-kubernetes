e173009baf4319cab74e8d891406d5d1acd2a9ea
Stephen Salamante <stephen_salamante@dlsu.edu.ph>
2020-11-29 02:39:24 +0800

    Merge branch 'sprint1' into user-profile

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,20 +1,56 @@
 // import module from db.js in models directory
 const database = require('../models/db.js');
+const HealthProgram = require('../models/HealthProgramModel.js');
 
 // define objects for client request functions for a certain path in the server
 const controller = {
 
     // Add controllers here
     getLanding: function (req, res) {
-        res.render('landing', {
-            // layout: '/layouts/main',
-            title: 'Home | DoloMed',
-            home_active: true,
-        })
+        if (req.cookies.user_sid && !req.session.user) {
+            res.render('landing', {
+                title: 'Home | DoloMed',
+                home_active: true,
+                user_active: true,
+            })
+        }
+        else {
+            res.render('landing', {
+                title: 'Home | DoloMed',
+                home_active: true,
+                user_active: false,
+            })
+        }
     },
 
-    
+    getLogOut: function (req, res) {
+        req.logout;
+        req.session.destroy(function (err) { });
+        // res.redirect('/')
+        res.redirect('back');
+    },
 
+    getHealthPrograms: function (req, res) {
+        database.findMany(HealthProgram, {}, {}, function (healthprogramsContent) {
+            if (req.cookies.user_sid && !req.session.user) {
+                res.render('hp_directory', {
+                    title: 'Health Programs | DoloMed',
+                    hp_active: true,
+                    user_active: true,
+                    healthprogramsContent: healthprogramsContent,
+                    test: "Test 1"
+                })
+            }
+            else {
+                res.render('hp_directory', {
+                    title: 'Health Programs | DoloMed',
+                    hp_active: true,
+                    healthprogramsContent: healthprogramsContent,
+                    test: "Test 2"
+                })
+            }
+        });
+    },
 }
 
 // enables to export controller object when called in another .js file
--- controllers/helper.js
+++ controllers/helper.js
@@ -1,10 +1,5 @@
-const database = require('../models/db.js');
-const sanitize = require('mongo-sanitize');
-
 const helper = {
-    sanitize: function (query) {
-        return sanitize(query);
-    }
+   
 }
 
 module.exports = helper;
