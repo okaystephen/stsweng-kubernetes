95001f8e8d6ed9fadd24d0ef714a2f2e48a172ff
Stephen Salamante <stephen_salamante@dlsu.edu.ph>
2020-12-01 22:34:21 +0800

    Update accountController.js

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,12 +1,23 @@
+const db = require('../models/db');
+const User = require('../models/UserModel');
+const moment = require('moment');
+
 const accountController = {
     // render account page when client requests '/account' defined in routes.js
     getAccount: function (req, res) {
-        res.render('account', {
-            layout: 'profile',
-            active_session: (req.session.user && req.cookies.user_sid),
-            active_user: req.session.user,
-            title: 'Account | DoliMed'
-        });
+        if (!req.session.user) res.redirect('/')
+        else {
+            db.findOne(User, { _id: req.session.user }, '', function (user) {
+                res.render('account', {
+                    layout: 'profile',
+                    active_session: (req.session.user && req.cookies.user_sid),
+                    active_user: req.session.user,
+                    title: 'Account | DoloMed',
+                    user: user.toObject(),
+                    date: moment(user.date).format('YYYY-MM-DD')
+                });
+            })
+        }
     },
 }
 
