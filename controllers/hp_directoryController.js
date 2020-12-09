7f159e01ded453ffd31c114c1f4f79d1a197895f
iravillanuevaa <ira_villanueva@dlsu.edu.ph>
2020-12-09 18:48:14 +0800

    updated hp_directory controller

--- controllers/hp_directoryController.js
+++ controllers/hp_directoryController.js
@@ -1,13 +1,15 @@
-const database = require('../models/db');
+const db = require('../models/db');
+const mongoose = require('mongoose');
 const User = require('../models/UserModel');
 const moment = require('moment');
+const sanitize = require('mongo-sanitize');
 const HealthProgram = require('../models/HealthProgramModel.js');
+const UserProgram = require('../models/UserHProgramModel.js');
 
 const hp_directoryController = {
     // render account page when client requests '/account' defined in routes.js
-   
     getHealthPrograms: function (req, res) {
-        database.findMany(HealthProgram, {}, {}, function (healthprogramsContent) {
+        db.findMany(HealthProgram, {}, {}, function (healthprogramsContent) {
             if (req.cookies.user_sid && req.session.user) {
                 res.render('hp_directory', {
                     layout: 'main',
@@ -29,6 +31,38 @@ const hp_directoryController = {
             }
         });
     },
+
+    postUserProgram: function (req, res){
+        console.log(req.params.hpId)
+        var reason = sanitize(req.body.reason);
+        var exposure = sanitize(req.body.exposure);
+
+
+        var program = new UserProgram({
+            _id: new mongoose.Types.ObjectId(),
+            program: req.params.hpId,
+            reason: reason,
+            exposure: exposure
+        })
+
+        console.log(program);
+        console.log(req.session.user);
+
+        db.insertOne(UserProgram, program, function(flag){
+            if(flag){
+                db.updateOne(User, {_id: req.session.user}, { $push: { programs: program._id} }, function (result){
+                    if(result){
+                        db.updateOne(HealthProgram, {_id: req.params.hpId},  { $push: { participants: req.session.user } }, function (result){
+                            if(result){
+                                res.redirect('/healthprograms')
+                            }
+                        })
+                    }
+                })
+            }
+        })
+
+    }
 }
 
 // enables to account controller object when called in another .js file
